fffa583d367cd0f947a49dca5e83478753b515d9
Sebastian Knudsen <sebastian.knudsen@nav.no>
2019-11-22 12:38:54 +0100

    fetching av status

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,6 +1,7 @@
 import FetchMock, { MiddlewareUtils } from 'yet-another-fetch-mock';
 import { nySykmeldingMock, avvistSykmeldingMock } from './data/sykmeldingMock';
 import naermesteLedereMock from './data/narmesteLedereMock';
+import { Status } from '../types/sykmeldingDataTypes';
 
 const mock = FetchMock.configure({
     enableFallback: true,
@@ -9,4 +10,5 @@ const mock = FetchMock.configure({
 
 mock.get('/syforest/sykmelding', nySykmeldingMock);
 mock.get('/syforest/sykmelding/avvist', avvistSykmeldingMock);
+mock.get('/syforest/sykmelding/:id/kvittering', { status: Status.AVVIST });
 mock.get('/syforest/naermesteledere', naermesteLedereMock);
--- src/pages/KvitteringSide.tsx
+++ src/pages/KvitteringSide.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router';
+import useFetch, { isNotStarted, hasFinished, hasFailed, FetchState, hasData } from '../hooks/useFetch';
 
 import Brodsmuler, { Brodsmule } from '../components/brodsmuler/brodsmuler';
 import useAppStore from '../store/useAppStore';
 import { Status } from '../types/sykmeldingDataTypes';
-
+import Spinner from 'nav-frontend-spinner';
 
 const brodsmuler: Brodsmule[] = [
     {
@@ -29,12 +31,38 @@ const brodsmuler: Brodsmule[] = [
 ];
 
 /*
- * Må ikke kunne aksessere denne siden med mindre status er satt som bekreftet eller sendt for den aktuelle id 
+ * Må ikke kunne aksessere denne siden med mindre status er satt som bekreftet eller sendt for den aktuelle id
  */
 
 const KvitteringSide = () => {
-    const { sykmelding, sykmeldingStatus } = useAppStore();
-    return <p>kvitteringside</p>    
-}
+    const { id } = useParams();
+    const statusFetcher = useFetch<{ status: Status }>();
+    const { sykmeldingStatus, setSykmeldingStatus } = useAppStore();
+
+    useEffect(() => {
+        if (isNotStarted(statusFetcher)) {
+            statusFetcher.fetch(
+                `/syforest/sykmelding/${id}/kvittering`,
+                undefined,
+                (fetchState: FetchState<{ status: Status }>) => {
+                    if (hasData(fetchState)) {
+                        const { data } = fetchState;
+                        setSykmeldingStatus(data.status);
+                    }
+                },
+            );
+        }
+    }, [id, setSykmeldingStatus, statusFetcher]);
+
+    if (!hasFinished(statusFetcher)) {
+        return <Spinner />;
+    }
+
+    if (hasFailed(statusFetcher)) {
+        return <p>failed to fetch status</p>;
+    }
+
+    return <p>{'status: ' + sykmeldingStatus}</p>;
+};
 
 export default KvitteringSide;
